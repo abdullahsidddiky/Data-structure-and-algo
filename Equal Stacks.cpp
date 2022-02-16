e4a52ee119d06aa528059ae7b8de86b7140200d8
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-02-17 01:51:18 +0600

    push

---
+++ Equal Stacks.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ stack<string> s;
+ string c;
+int n ;
+cin>>n;
+while(n--){
+    cin>>c;
+    for(int i = 0; i<c.length();i++){
+        cout<<i<<" " ;
+        cout<<endl;
+
+    }
+}
+}
--- Reverse a stack using recursion.cpp
+++ Reverse a stack using recursion.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void stackreverse(stack<int> s){
+    int value;
+    value = s.top();
+    s.pop();
+    stackreverse(s);
+    s.push(value);
+};
+int main(){
 
+stack <int> st;
+int n;
+cin>>n;
+int v;
+
+while(n--){
+    cin>>v;
+    st.push(v);
+}
+while(st.empty() == false){
+    cout<<st.top()<<" ";
+    st.pop();
+}
+
+}
