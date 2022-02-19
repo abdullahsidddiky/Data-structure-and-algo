d87d8dbea1ba3da29c0238faefc9c762fe81853e
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-02-19 21:23:29 +0600

    reverse stack complete

--- Reverse a stack.cpp
+++ Reverse a stack.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-stack <int> st;
-void stackreverse(){
+
+void insertatbottom(stack <int> &s, int k){
+if (s.empty()){
+    s.push(k);
+    return;
+}
+    int temp = s.top();
+    s.pop();
+    insertatbottom(s,k);
+    s.push(temp);
+
+}
+void stackreverse(stack <int> &st){
    if(st.empty()){
     return;
    }
    int value;
    value = st.top();
    st.pop();
-   stackreverse();
-   st.push(value);
+   stackreverse(st);
+   insertatbottom(st ,value);
+
 
 };
 int main(){
-int n;
-cin>>n;
-int v;
+    stack<int> st;
+    int n;
+    cin>>n;
+    int v;
 
 while(n--){
     cin>>v;
     st.push(v);
 }
-stackreverse();
+stackreverse(st);
 
 while(st.empty() == false){
     cout<<st.top()<<" ";
