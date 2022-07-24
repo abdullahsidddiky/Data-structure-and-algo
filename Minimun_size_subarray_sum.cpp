db8eab87865da507e2221825f1815d05dc5ddb7e
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2022-07-25 04:09:08 +0600

    m

---
+++ Minimun_size_subarray_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int target;
+    cin>>target;
+    int n;
+    cin>>n;
+    int arr[n];
+    int sum=1000;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int start=0;
+    int end=0;
+   
+
+}
