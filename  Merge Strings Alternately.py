27b5d19e97b25051f9bb1095fc2f10d90a9d88b1
abdullahsiddiky <mohammad.siddiky@northsouth.edu>
2024-06-23 03:13:02 +0600

    m

---  Merge Strings Alternately.py
+++  Merge Strings Alternately.py
@@ -1,12 +1,16 @@
-word1 = input('word?')
-word2= input('word2?')
-i,j=0,0
-res=[]
-while(i<len(word1) and j<len(word2)):
-   res.append(word1[j]) 
-   res.append(word2[j])
-   i+=1
-   j+=1
-res.append(word1[i:])
-res.append(word2[j:])
-print("".join(res))
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result=''
+        i=j=0
+        while  i < len(word1) and j < len(word2):
+            result+= word1[i]+ word2[j]
+            i +=1
+            j +=1
+        while i < len(word1):
+            result+=word1[i]
+            i+=1
+        while j < len(word2):
+            result+=word2[j]
+            j+=1
+        return result
+        
